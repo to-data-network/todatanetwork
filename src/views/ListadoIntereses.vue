f8aa4483bc905d8852d738ae89f62c473f6e4540
kavanegasm <kavanegasm@unal.edu.co>
2019-07-08 20:59:34 -0500

    other changes

---
+++ src/views/ListadoIntereses.vue
@@ -0,0 +1,58 @@
+
+<template>
+<div>
+      <base-header type="gradient-success" class="pb-6 pb-6 pt-5 pt-md-8">
+            <div class="row">
+                
+                <p>En el presente es un listado que comprende las actividades que se realizan a nivel  local, señale el ítem correspondiente a la actividad que la persona realiza en el espacio correspondiente a la frecuencia con la que puede realizarla, y al nivel de satisfacción de la persona al realizarla </p>
+                <b-table striped hover :items="tipo"></b-table>
+            </div>
+        </base-header>
+
+         <div class="container-fluid mt--7">
+             <h2>hola</h2>
+           <b-table striped hover :items="items"></b-table>
+           <h1>holaaaa</h1>
+           <b-form-input id="range-1" v-model="value" type="range" min="1" max="5"></b-form-input>
+           <div class="mt-2">Value: {{ value }}</div>
+            </div>
+            
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ],
+        tipo: [
+          { frecuencia: 'Siempre (S): 4 o más días por semana.', nivel_satisfaccion: 'Muy Satisfactoria (MS)' },
+          { frecuencia: 'Frecuentemente (F): 1 vez cada quince días.', nivel_satisfaccion: 'Satisfactoria (S)'},
+          { frecuencia: 'A veces (AV): 1 vez por mes', nivel_satisfaccion: 'Agradable (A)' },
+          { frecuencia: 'Rara vez (R): 2 o 3 veces al año ', nivel_satisfaccion: 'Poco agradable (PA)' }
+        ],
+        value: '2'
+      }
+      
+    }
+  }
+</script>
+
+<style scoped>
+.table {
+	width: 100%;
+	margin-bottom: 1rem;
+	color: #525f7f;
+	background-color: #81464600;
+    border: 1px solid #dee2e6;
+}
+</style>
+
--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -1,30 +1,68 @@
-<template>
-     <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-            
-            <div class="row">
-              <select v-model="selected" multiple>
-  <option>A</option>
-  <option>B</option>
-  <option>C</option>
-</select>
-<br>
-<span>Selected: {{ selected }}</span>
-<b-container fluid>
-    <b-row class="my-1" v-for="type in types" :key="type">
-      <b-col sm="3">
-        <label :for="`type-${type}`">Type {{ type }}:</label>
-      </b-col>
-      <b-col sm="9">
-        <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
-      </b-col>
-    </b-row>
-  </b-container>
-              </div>
-        </base-header>
-            
 
+<template>
+<div>
+      <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+            <div class="row">
+                
+                <p>En el presente es un listado que comprende las actividades que se realizan a nivel  local, señale el ítem correspondiente a la actividad que la persona realiza en el espacio correspondiente a la frecuencia con la que puede realizarla, y al nivel de satisfacción de la persona al realizarla </p>
+                <b-table striped hover :items="tipo"></b-table>
+            </div>
+        </base-header>
+
+         <div class="container-fluid mt--7">
+           
+           
+           <b-form-group label="Selection mode:" label-cols-md="4">
+      <b-form-select v-model="selectMode" :options="modes" class="mb-3"></b-form-select>
+    </b-form-group>
+
+    <b-table
+      selectable
+      :select-mode="selectMode"
+      selectedVariant="success"
+      :items="items"
+      :fields="fields"
+      @row-selected="rowSelected"
+    >
+      <!-- Example scoped slot for illustrative purposes only -->
+      <template slot="selected" slot-scope="{ rowSelected }">
+        <span v-if="rowSelected">✔</span>
+      </template>
+    </b-table>
+
+    {{ selected }} 
+     </div> 
+  </div>
 </template>
 <script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
+  export default {
+    data() {
+      return {
+        modes: ['multi', 'single', 'range'],
+        fields: ['selected', 'isActive', 'age', 'first_name', 'last_name'],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ],
+        selectMode: 'multi',
+        selected: []
+      }
+    },
+    methods: {
+      rowSelected(items) {
+        this.selected = items
+      }
+    }
+  }
 </script>
+
+<style scoped>
+
+</style>
 
